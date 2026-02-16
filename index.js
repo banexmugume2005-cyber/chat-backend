4e2cedbdddf5cd93a5a48bcc4e523975458c72bb
banexmugume2005-cyber <banexmugume2005@gmail.com>
2026-02-16 12:45:52 +0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const app = express();
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Chat Backend Running 🚀");
+});
+
+app.post("/chat", (req, res) => {
+  const message = req.body.message;
+
+  res.json({
+    reply: "You said: " + message
+  });
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log("Server running on port " + PORT);
+});
